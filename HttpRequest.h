4715a1c13d9be7d8606e569a952188a14e3c97d6
devinbedari <devinbedari@msn.com>
2016-05-01 14:16:35 -0700

    Corrected an unmerged file
    HttpRequest.h was not merged properly

--- HttpRequest.h
+++ HttpRequest.h
@@ -4,14 +4,6 @@
 #include <cstring>
 #include <string>
 
-<<<<<<< HEAD
-class HttpGetRequest {
-protected:
-	char* path;
-	char* host;
-	char protocolVersion[9];
-	char* getReq;
-=======
 class HttpGetRequest 
 {
 protected:
@@ -25,36 +17,24 @@ protected:
 	void helper(char* buffer, char* &dataToStore);
 	// Clear cstrings
 	void clear();
->>>>>>> devin
 
 public:
 	// Default Constructor
 	HttpGetRequest();
 	// Parameterized Constructor
 	HttpGetRequest(char* fpath, char* fhost);
-<<<<<<< HEAD
-=======
 	// Destructor
 	~HttpGetRequest();
->>>>>>> devin
 	// Generate the HTTP request
 	char* genReq();
 	// Parse request
 	void parseReq(char *buffer);
-<<<<<<< HEAD
-	// Clear cstrings
-	void clear();
-=======
->>>>>>> devin
 
   	// Return protected variables
 	char* getPath();
   	char* getHost();
   	char* getProtocolVersion();
-<<<<<<< HEAD
-=======
   	// int getErrorStatus();
->>>>>>> devin
 };
 
 #endif
